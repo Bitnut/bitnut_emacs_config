361041bcf9fee7374bf54329f40228b3a2c942de
huangpinchao <huangpc@sz-bcs.com.cn>
2020-09-02 19:01:39 +0800

    add settings for dired icons

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -4,67 +4,68 @@
 
 (when (>= emacs-major-version 24)
   ;;(require 'package)
- 
+  
   (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-		      ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
+			   ("melpa" . "http://elpa.emacs-china.org/melpa/"))))
 ;; Add Packages
 (defvar my/packages '(
 		      ;; --- Auto-completion ---
-		       company		       
-		       ;; --- Better Editor ---		       	       
-		       hungry-delete
-		       swiper
-		       counsel
-		       smartparens
-		       ;; --- Major Mode ---
-		       js2-mode
-		       ;; --- Minor Mode ---
-		       nodejs-repl
-		       exec-path-from-shell
-		       ;; --- popwin ---
-		       popwin
-		       ;; --- Themes ---
-		       ;;monokai-theme
-		       spacemacs-theme
-		       emmet-mode
-		       js2-refactor
-		       web-mode
-		       expand-region
-		       iedit
-		       helm-ag
-		       ;;pyim input method
-		       pyim
-		       pyim-basedict
-		       ;;typescritpt
-		       typescript-mode
-		       ;; flycheck
-		       flycheck
-		       flycheck-posframe
-		       flycheck-pos-tip
-		       flycheck-popup-tip
-		       ;; magit
-		       magit
-		       use-package
-		       rjsx-mode
-		       tide
-		       ;; pyim 弹窗
-		       posframe
-		       ;; markdown
-		       markdown-mode
-		       ;; esh-doc
-		       esh-help
-		       eshell-z
-		       eshell-prompt-extras
-		       ;; shell
-		       shell
-		       xterm-color
-		       shell-pop
-		       ;; lsp
-		       lsp-mode
-		       ;; icons
-		       all-the-icons
-		       spaceline-all-the-icons
-		       ) "Default packages")
+		      company
+		      ;; --- Better Editor ---		       	       
+		      hungry-delete
+		      swiper
+		      counsel
+		      smartparens
+		      ;; --- Major Mode ---
+		      js2-mode
+		      ;; --- Minor Mode ---
+		      nodejs-repl
+		      exec-path-from-shell
+		      ;; --- popwin ---
+		      popwin
+		      ;; --- Themes ---
+		      ;;monokai-theme
+		      spacemacs-theme
+		      emmet-mode
+		      js2-refactor
+		      web-mode
+		      expand-region
+		      iedit
+		      helm-ag
+		      ;;pyim input method
+		      pyim
+		      pyim-basedict
+		      ;;typescritpt
+		      typescript-mode
+		      ;; flycheck
+		      flycheck
+		      flycheck-posframe
+		      flycheck-pos-tip
+		      flycheck-popup-tip
+		      ;; magit
+		      magit
+		      use-package
+		      rjsx-mode
+		      tide
+		      ;; pyim 弹窗
+		      posframe
+		      ;; markdown
+		      markdown-mode
+		      ;; esh-doc
+		      esh-help
+		      eshell-z
+		      eshell-prompt-extras
+		      ;; shell
+		      shell
+		      xterm-color
+		      shell-pop
+		      ;; lsp
+		      lsp-mode
+		      ;; icons
+		      all-the-icons
+		      spaceline-all-the-icons
+		      all-the-icons-dired
+		      ) "Default packages")
 
 (setq package-selected-packages my/packages)
 
@@ -134,23 +135,23 @@
   :init (load-theme 'spacemacs-dark t))
 
 (defun js2-imenu-make-index ()
-      (interactive)
-      (save-excursion
-	;; (setq imenu-generic-expression '((nil "describe\\(\"\\(.+\\)\"" 1)))
-	(imenu--generic-function '(("describe" "\\s-*describe\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-				   ("it" "\\s-*it\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-				   ("test" "\\s-*test\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-				   ("before" "\\s-*before\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-				   ("after" "\\s-*after\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
-				   ("Function" "function[ \t]+\\([a-zA-Z0-9_$.]+\\)[ \t]*(" 1)
-				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
-				   ("Function" "^var[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
-				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*()[ \t]*{" 1)
-				   ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*:[ \t]*function[ \t]*(" 1)
-				   ("Task" "[. \t]task([ \t]*['\"]\\([^'\"]+\\)" 1)))))
+  (interactive)
+  (save-excursion
+    ;; (setq imenu-generic-expression '((nil "describe\\(\"\\(.+\\)\"" 1)))
+    (imenu--generic-function '(("describe" "\\s-*describe\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+			       ("it" "\\s-*it\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+			       ("test" "\\s-*test\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+			       ("before" "\\s-*before\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+			       ("after" "\\s-*after\\s-*(\\s-*[\"']\\(.+\\)[\"']\\s-*,.*" 1)
+			       ("Function" "function[ \t]+\\([a-zA-Z0-9_$.]+\\)[ \t]*(" 1)
+			       ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
+			       ("Function" "^var[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*=[ \t]*function[ \t]*(" 1)
+			       ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*()[ \t]*{" 1)
+			       ("Function" "^[ \t]*\\([a-zA-Z0-9_$.]+\\)[ \t]*:[ \t]*function[ \t]*(" 1)
+			       ("Task" "[. \t]task([ \t]*['\"]\\([^'\"]+\\)" 1)))))
 (add-hook 'js2-mode-hook
-	      (lambda ()
-		(setq imenu-create-index-function 'js2-imenu-make-index)))
+	  (lambda ()
+	    (setq imenu-create-index-function 'js2-imenu-make-index)))
 
 ;; call imenu to list functions
 (global-set-key (kbd "M-s i") 'counsel-imenu)
@@ -166,17 +167,16 @@
   :config
   (setq helm-ag-use-grep-ignore-list t)
   )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; all-the icons/modeline;;remember to call 'M-x all-the-icons-install-fonts'
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package all-the-icons)
-;; remember to call 'M-x all-the-icons-install-fonts'
-;;
-;;	     :ensure t
- ;; :defer t
-  ;;:init (set-frame-font "all-the-icons" t)
-  ;;:config (set-frame-font "Source Code Pro" t))
-
-
 (use-package spaceline-all-the-icons
   :config (spaceline-all-the-icons-theme)
-  (setq spaceline-all-the-icons-separator-type 'slant))
+  (setq spaceline-all-the-icons-separator-type 'slant)
+  (setq separator-scale '1)
+  )
+(add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
 
 (provide 'init-packages)
